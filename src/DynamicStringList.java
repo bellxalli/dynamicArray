dc5e514d1db113a92886fdec5da9ecc25575d6fd
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-04-08 12:15:35 -0700

    added structure for implementing methods in dynamicStringList.java

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,31 @@
-public class DynamicStringList  {
-    
+public class DynamicStringList implements StringList {
+    public String get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void set(int index, String value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void add(String value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public String remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int capacity() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 }
