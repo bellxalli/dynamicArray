3ca5cf8427fc1ee09c5d84f5b6b42f70307ce802
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-04-08 17:21:28 -0700

    added test 'removeShiftsElementsLeft'

--- src/test.java
+++ src/test.java
@@ -19,4 +19,27 @@ public class test {
 
     }//end addPlacesValueAtEnd
 
+    @Test
+        public void removeShiftsElementsLeft() {
+        // Arrange
+            DynamicStringList list = new DynamicStringList();
+            list.set(0, "A");
+            list.set(1, "B");
+            list.set(2, "C");
+
+            int originalSize = list.size();
+
+        // Act
+            String removed = list.remove(0);
+
+        // Assert
+            assertEquals("A", removed); // removed index 0, "A"
+            assertEquals("B", list.get(0)); // B shifted to index 0
+            assertEquals("C", list.get(1)); // C shifted to index 1
+            assertEquals(originalSize - 1, list.size()); // expected res: originalSize is decreased by 1
+
+    }//end removeShiftsElementsLeft
+
+
+
 }//end test
