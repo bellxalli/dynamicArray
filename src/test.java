0c05f41b8d5fc38613daea5dfbea0602ea67cad4
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-04-08 17:08:11 -0700

    added test 'addPlacesValueAtEnd'

--- src/test.java
+++ src/test.java
@@ -1,3 +1,22 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class test {
     
-}
+    @Test
+        public void addPlacesValueAtEnd() {
+        // Arrange
+            DynamicStringList list = new DynamicStringList();
+            int last = list.capacity() - 1;
+            list.set(last, "old"); // should fill last element
+            String addingNew = "new";
+    
+        // Act
+            list.add(addingNew);
+    
+        // Assert
+            assertEquals(addingNew, list.get(last)); //expected res: new (addingNew)
+
+    }//end addPlacesValueAtEnd
+
+}//end test
